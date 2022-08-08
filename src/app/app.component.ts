3e5f52803d4d2cb37eab4f675291eb9a29fc7f33
paulripault <paulripault0@gmail.com>
2022-08-08 16:06:04 +0200

    generation of the map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'cooking-map';
+
+  ngOnInit(): void {
+    let loader = new Loader({
+      apiKey: 'AIzaSyBvzomIKwgvGfO5E0ulL-XgExoAta1MjpI'
+    })
+
+    loader.load().then(() => {
+      const location = { lat: 51.233334, lng: 	6.783333 }
+
+      new google.maps.Map(document.getElementById("map")!, {
+        center: location,
+        zoom: 6
+      })
+    })
+  }
+
 }
